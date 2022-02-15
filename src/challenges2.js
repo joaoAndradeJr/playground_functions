cb1dbb73a008dc7b38135b4b5ed600e2152b1cd0
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-15 12:58:22 -0400

    feat: desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -66,8 +66,16 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(str) {
+  let regex = /[0-9]/g;
+  let values = str.match(regex);
+  let sum = 0;
+
+  for (let i = 0; i < values.length; i += 1) {
+    sum += Number(values[i]);
+  }
+
+  return `${sum} copos de água`;
 }
 
 module.exports = {
