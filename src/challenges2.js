3f17c8d9bb960022acb06da84d4888bf98614ae2
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-15 10:07:23 -0400

    add: generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,10 +11,33 @@ function techList(array, person) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let phone = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (i === 0) {
+      phone.push('(');
+      phone.push(array[i]);
+    } else if (i === 2) {
+      phone.push(')');
+      phone.push(' ');
+      phone.push(array[i]);
+    } else if (i === 7) {
+      phone.push('-');
+      phone.push(array[i]);
+    } else {
+      phone.push(array[i]);
+    }
+  }
+  return phone.join('');
 }
 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
